47b0526de72a640f8aa60af13c287dbc64f90bdb
unocabs <rgiancabrera@gmail.com>
2023-04-07 20:57:10 +0800

    Final Done

--- src/main/java/com/unocabs/phonebrands/controller/PhoneController.java
+++ src/main/java/com/unocabs/phonebrands/controller/PhoneController.java
@@ -1,11 +1,27 @@
 package com.unocabs.phonebrands.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.unocabs.phonebrands.DTO.PhoneDTO;
+import com.unocabs.phonebrands.model.PhoneModel;
+import com.unocabs.phonebrands.service.PhoneService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/phones")
+@RequiredArgsConstructor
 public class PhoneController {
+
+    private final PhoneService phoneService;
+
+    @GetMapping
+    public List<PhoneDTO> getAllPhone() {
+        return phoneService.getAllPhone();
+    }
+
+    @PostMapping
+    public List<PhoneDTO> postPhone(@RequestBody PhoneModel phoneModel) {
+        return phoneService.postPhone(phoneModel);
+    }
 }
--- src/main/java/com/unocabs/phonebrands/service/PhoneService.java
+++ src/main/java/com/unocabs/phonebrands/service/PhoneService.java
@@ -7,6 +7,7 @@ import com.unocabs.phonebrands.repository.PhoneRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,6 +17,21 @@ public class PhoneService {
 
     private final PhoneRepository phoneRepository;
 
+    public List<PhoneDTO> getAllPhone() {
+        List<PhoneEntity> phoneEntities = phoneRepository.findAll();
+        List<PhoneDTO> phoneDTOList = new ArrayList<>();
+
+        phoneEntities.forEach(phoneEntity -> phoneDTOList.add(PhoneDTO.builder()
+                        .phoneName(phoneEntity.getPhoneName())
+                        .brandName(phoneEntity.getBrandName())
+                        .price(phoneEntity.getPrice())
+                .build()));
+
+
+        return phoneDTOList;
+    }
+
+
     public List<PhoneDTO> postPhone (PhoneModel phoneModel) {
         phoneRepository.save(PhoneEntity.builder()
                         .phoneId(UUID.randomUUID())
@@ -23,6 +39,6 @@ public class PhoneService {
                         .brandName(phoneModel.getBrandName())
                         .price(phoneModel.getPrice())
                 .build());
-        return null;
+        return getAllPhone();
     }
 }
